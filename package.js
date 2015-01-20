d1d34ce9b741f5b3bcd546b8e7891458640c502d
Lt <trepafi@gmail.com>
2015-01-20 12:13:58 +0100

    Updating package version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: "trepafi:semantic-ui",
     summary: "Semantic UI for Meteor",
-    version: "0.1.1",
+    version: "0.2.0",
     git: "https://github.com/trepafi/meteor-semantic-ui.git"
 });
 
